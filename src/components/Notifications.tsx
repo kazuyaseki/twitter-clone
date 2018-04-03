74a80b7acde4d70cd3a0cbd5b3fc0e2f9033f6fe
kazuya-seki <track613armory@gmail.com>
2018-04-03 21:31:59 +0900

    added Notification component

---
+++ src/components/Notifications.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+const Notifications = (props: any) => (
+  <div>
+    <nav>
+      <span>すべて</span>
+      <span>@ツイート</span>
+    </nav>
+    <ul>
+      <li>a</li>
+    </ul>
+  </div>
+);
+
+export default Notifications;
