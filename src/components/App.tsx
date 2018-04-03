ce3cbfa223d410d44362614f60a7582415e041b6
kazuya-seki <track613armory@gmail.com>
2018-04-03 21:32:17 +0900

    added path for notification

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 
-import Home from './Home';
 import Header from './organisms/Header';
+import Home from './Home';
+import Notifications from './Notifications';
 
 class App extends React.Component {
   render() {
@@ -11,7 +12,8 @@ class App extends React.Component {
       <div className="App">
         <Header />
         <main>
-          <Route path="/" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route path="/notifications" component={Notifications} />
         </main>
       </div>
     );
