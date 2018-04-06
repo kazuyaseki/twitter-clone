bb65494e4ab21cb2b6ec134a76b6af1af4a589f1
kazuya-seki <track613armory@gmail.com>
2018-04-06 23:22:01 +0900

    added style to sidebar profile card

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -2,25 +2,25 @@ import * as React from 'react';
 import './Sidebar.css';
 
 const Sidebar = (props: any) => (
-  <div>
+  <div className="sidebar">
     <div className="user-profile">
       <img src="#" className="header-img" />
       <img src="#" className="icon" />
 
       <div className="user-field">seya</div>
 
-      <ul>
+      <ul className="profile-stats">
         <li>
-          <span>ツイート</span>
-          <span>725</span>
+          <span className="profile-stats-title">ツイート</span>
+          <span className="profile-stats-value">725</span>
         </li>
         <li>
-          <span>フォロー</span>
-          <span>725</span>
+          <span className="profile-stats-title">フォロー</span>
+          <span className="profile-stats-value">725</span>
         </li>
         <li>
-          <span>フォロワー</span>
-          <span>725</span>
+          <span className="profile-stats-title">フォロワー</span>
+          <span className="profile-stats-value">725</span>
         </li>
       </ul>
     </div>
